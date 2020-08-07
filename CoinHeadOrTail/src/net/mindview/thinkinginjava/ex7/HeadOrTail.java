61b055df5b4d0052ec739e3ba3a291513ed68190
Didar Akulov <tam-tam@bk.ru>
2020-08-07 17:28:06 +0600

    initial commit

---
+++ CoinHeadOrTail/src/net/mindview/thinkinginjava/ex7/HeadOrTail.java
@@ -0,0 +1,18 @@
+package net.mindview.thinkinginjava.ex7;
+
+public class HeadOrTail {
+
+	
+	public static void main(String[] args) {
+		double coin = Math.random();
+		
+		if (coin > 0.5) {
+			System.out.println("Head");
+		} else {
+			System.out.println("Tail");
+		}
+		
+
+	}
+
+}
